199cb441f4db6ee79379208211a9e50ca6e5ba9d
alexeik <transmissive.trash@gmail.com>
2024-03-31 02:58:02 +0300

    compiled.tested.

--- src/main/java/com/mrtrollnugnug/ropebridge/handler/LadderBuildingHandler.java
+++ src/main/java/com/mrtrollnugnug/ropebridge/handler/LadderBuildingHandler.java
@@ -19,7 +19,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.fmllegacy.server.ServerLifecycleHooks;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 
 import java.util.Timer;
